4920703b148427291ad59685a5de59f7d0ac3f81
socialguard <48410519+socialguard@users.noreply.github.com>
2021-02-06 23:25:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import wakeonlan
+from wakeonlan import send_magic_packet
 from getmac import get_mac_address
-ip_mac = get_mac_address(ip="192.168.1.100")
-print(ip_mac)
-wakeonlan.create_magic_packet(ip_mac)
+import time
+ipp="192.168.1.100"
+ip_mac = get_mac_address(ipp)
+def slp(s):
+    time.sleep(s)
+print("Trying to wake up "+ip_mac+"...")
+slp(1)
+try:
+    send_magic_packet(ip_mac, ip_address="192.168.1.100", port=9)
+except TypeError:
+    print("Not founded object by ip "+ipp)
+else:
+    print("Successful!")
